89a3e99e072d24a177b043a69aa42e7c02278947
wextia <valcazar96@gmail.com>
2016-07-07 11:36:23 +0200

    Player Movement, shooting and collisions

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 5;
+    private new Rigidbody rigidbody;
+    float deltaSpeed;
+    // Use this for initialization
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        deltaSpeed = speed * Time.fixedDeltaTime;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        rigidbody.MovePosition(
+            transform.position +
+            new Vector3(
+                deltaSpeed * Input.GetAxis("Horizontal"),
+                0,
+                deltaSpeed * Input.GetAxis("Vertical")
+                )
+            );       
+    }
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+
+    private float cameraHeight;
+    private Transform playerGeom;
+    [SerializeField]
+    GameObject bulletPrefab;
+    Transform muzzle;
+
+    void Start() {
+        playerGeom = transform.GetChild(0);
+        cameraHeight = Camera.main.transform.position.y - playerGeom.position.y;
+        muzzle = transform.GetChild(0).transform.GetChild(0);
+    }
+
+    void Update () {
+        Vector3 mouse = Input.mousePosition;
+        mouse.z = cameraHeight;
+        playerGeom.LookAt(Camera.main.ScreenToWorldPoint(mouse));
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletPrefab, muzzle.transform.position, playerGeom.transform.localRotation);
+        }
+    }
+}
