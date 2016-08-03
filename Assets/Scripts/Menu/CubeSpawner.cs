945cae2d791857af192bd6e2bf5d70ed90390d7c
wextia <valcazar96@gmail.com>
2016-08-03 17:54:23 +0200

    vi meid som xanjes

--- Assets/Scripts/Menu/CubeSpawner.cs
+++ Assets/Scripts/Menu/CubeSpawner.cs
@@ -22,7 +22,7 @@ public class CubeSpawner : MonoBehaviour {
             Vector3 randomSpawnPos = new Vector3(Random.Range(-spawnWidth / 2, spawnWidth / 2),
                 transform.position.y, Random.Range(-spawnHeight / 2, spawnHeight / 2));
             GameObject cube = 
-            Instantiate(cubePrefab, randomSpawnPos, Random.rotation) as GameObject;
+            Instantiate(cubePrefab, randomSpawnPos + transform.position, Random.rotation) as GameObject;
             cube.GetComponent<MeshRenderer>().material.color = Random.ColorHSV(0,1,1,1,1,1);
             yield return new WaitForSeconds(timeBetweenCubes);
         }
---
+++ Assets/TakeAScreenshot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TakeAScreenshot : MonoBehaviour {
+    [SerializeField]
+    KeyCode keyToPress = KeyCode.K;
+    [SerializeField]
+    int scaleFactor = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(keyToPress))
+        {
+            string screenshotName = "Screenshot-" + System.DateTime.Now.ToFileTime() + ".png";
+            Debug.Log(Application.persistentDataPath + screenshotName);
+            Application.CaptureScreenshot(screenshotName, scaleFactor);
+        }
+	}
+}
